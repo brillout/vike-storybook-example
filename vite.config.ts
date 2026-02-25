2c34ab45ba2a7e2267a336e302c1e82a1c298998
Romuald Brillout <git@brillout.com>
2026-02-25 12:57:47 +0100

    Revert "simple workaround"

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,10 @@ import { fileURLToPath } from 'node:url';
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 import { playwright } from '@vitest/browser-playwright';
 const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
-console.log('process.env.NODE_ENV', process.env.NODE_ENV)
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [
-    process.env.NODE_ENV !== 'storybook' && vike(),
-    react()
-  ],
+  plugins: [vike(), react()],
   test: {
     projects: [{
       extends: true,
